37e6b5457acde4df00a6d8e81ada1d11c425b9be
森本拓朗 <ujimattya.mt@gmail.com>
2021-06-29 13:05:26 +0900

    ✨ Header部分の作成とtailwindの設定追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,16 @@
-export default function Home() {
+import { NextPage } from 'next';
+
+const Index: NextPage = () => {
   return (
-    <div className="container">
-      <div className="w-full bg-snow-900 h-header"></div>
-      <div className="w-full sm:h-screen bg-snow-800 h-[48px] sm:w-[72px]"></div>
+    <div>
+      <div className="fixed top-0 z-40 w-full h-header bg-snow-900"></div>
+      <div className="md:flex z-30 md:flex-row mt-header">
+        <div className="hidden md:block md:w-[72px] md:h-menu bg-snow-900 "></div>
+        <div className="w-full md:w-[230px] h-[48px] md:h-menu bg-night-900"></div>
+        {/* <div className="w-[100px] h-[50px] bg-gray-100"></div> */}
+      </div>
     </div>
   );
-}
+};
+
+export default Index;
