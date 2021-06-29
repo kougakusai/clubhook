b5ce5b8ba572ea79cbf516a6f62c94972421c448
森本拓朗 <ujimattya.mt@gmail.com>
2021-06-29 13:11:33 +0900

    🚧 #4 issueに紐付けテスト

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ const Index: NextPage = () => {
       <div className="md:flex z-30 md:flex-row mt-header">
         <div className="hidden md:block md:w-[72px] md:h-menu bg-snow-900 "></div>
         <div className="w-full md:w-[230px] h-[48px] md:h-menu bg-night-900"></div>
-        {/* <div className="w-[100px] h-[50px] bg-gray-100"></div> */}
+        <div className="w-[100px] h-[50px] bg-gray-100"></div>
       </div>
     </div>
   );
